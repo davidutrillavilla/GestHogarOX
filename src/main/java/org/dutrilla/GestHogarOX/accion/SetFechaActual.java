3347108ed190f8ed06b625fd2a9c3f9ae68fe90d
dutrilla <davidutrillavilla@hotmail.com>
2026-02-06 19:12:02 +0100

    fix filtro gasto: filtro de la vista gasto

---
+++ src/main/java/org/dutrilla/GestHogarOX/accion/SetFechaActual.java
@@ -0,0 +1,20 @@
+package org.dutrilla.GestHogarOX.accion;
+
+import org.openxava.actions.BaseAction;
+
+
+public class SetFechaActual extends BaseAction {
+
+    public void execute() throws Exception {
+
+        int ejercicio = java.time.LocalDate.now().getYear();
+
+        int mes = java.time.LocalDate.now().getMonthValue();
+        // Esto crea y guarda la variable en la sesión aunque no esté en el XML
+        getContext().put(getManager().getApplicationName(), null, "ejercicioActual", ejercicio);
+
+        getContext().put(getManager().getApplicationName(), null, "mesActual", mes);
+
+
+    }
+}
